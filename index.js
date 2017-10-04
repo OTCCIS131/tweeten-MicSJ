a74c02e4d071766c28052b479918ff75264ff0dc
Unknown <mj0857363@otc.edu>
2017-10-04 13:20:55 -0500

    jquery

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-let btns = Array.from(document.getElementsByClassName('btn-inc'))
+// let btns = Array.from(document.getElementsByClassName('btn-inc'))
 
-btns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        let currentCount = btn.nextSibling
-        let newCount = parseInt(currentCount.textContent) + 1
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         let currentCount = btn.nextSibling
+//         let newCount = parseInt(currentCount.textContent) + 1
 
-        btn.nextSibling.textContent = ` ${newCount} `
+//         btn.nextSibling.textContent = ` ${newCount} `
+//     })
+// })
+
+$(function(){
+    $('.btn-inc').click(function(event){
+        let currentCount = $(this).next().text()
+        let newCount = parseInt(currentCount.text()) + 1
+        currentCount.text(currentCount.replacewith(newCount))
     })
-})
+});
