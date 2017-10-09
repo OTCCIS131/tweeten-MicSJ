da57fe4d4f5d3d199f3bf3a59a3cb28d447a40a7
Unknown <mj0857363@otc.edu>
2017-10-09 13:18:04 -0500

    Start of adding tweets

--- index.js
+++ index.js
@@ -10,9 +10,15 @@
 // })
 
 $(function(){
+    //Incrementing like button
     $('.btn-inc').click(function(event){
         let currentCount = $(this).next().text()
         newCount = parseInt(currentCount) + 1
         $(this).next().text(newCount)
+    })
+
+    $('.tweets').submit(function(e){
+        let tweet = $(this).text()
+        
     })
 });
