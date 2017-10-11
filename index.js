77bd16aeb847d229c6b643e557de4624db35ae18
Unknown <mj0857363@otc.edu>
2017-10-11 13:19:38 -0500

    meeeeh

--- index.js
+++ index.js
@@ -9,18 +9,34 @@
 //     })
 // })
 
-$(function(){
+$(function () {
     //Incrementing like button
-    $('.btn-inc').click(function(event){
+    $('.btn-inc').click(function (event) {
         let currentCount = $(this).next().text()
         newCount = parseInt(currentCount) + 1
         $(this).next().text(newCount)
     })
 
-    $('.tweets').submit(function(event){
-        let tweet = $(this).text()
-        let tweetClone = $('.original').clone()
-
+    $('.tweets').submit(function (event) {
         event.preventDefault()
+
+        let newTweet = $("[name=newTweet]", $(this))
+        let tweet = newTweet.val()
+
+        //ends function
+        if (tweet.trim() == '') return
+
+        let newElement = ($(".original").clone()).insertBefore($(".orginal"))
+
+        // newElement.text(newElement.words)
+
+        // let newElement = $("<div>")
+        // newElement.addClass('panel panel-default')
+        // newElement.text(tweet)
+        // $(".orginal").append(newElement)
+
+        newTweet = newTweet.val(' ')
+        // let tweetClone = $('.original').clone()
+
     })
 });
